147b5ae849b6f30469a71676084ca4f1a82e3a22
zhihanggg <zhihangtzh@gmail.com>
2025-03-30 14:20:56 +0800

    feat: commit结构

--- cmd/commit/commit.go
+++ cmd/commit/commit.go
@@ -27,60 +27,80 @@ type commitImpl struct {
 
 func (i *commitImpl) run() func(cmd *cobra.Command, args []string) error {
 	return func(cmd *cobra.Command, args []string) error {
-		// 检查是否支持Windows
-		if err := utils.CmdNotSupportWindowsIntercept("commit"); err != nil {
-			return err
-		}
-
-		// 扫描doc/docx文件
-		log.Debug("开始扫描文档文件...")
-		docFiles, err := utils.ScanFilesByExt(".", []string{".doc", ".docx"})
-		if err != nil {
-			return fmt.Errorf("扫描文档文件失败: %v", err)
-		}
-
-		if len(docFiles) == 0 {
-			log.Debug("未找到需要转换的文档文件")
-		} else {
-			log.Debug("找到 %d 个文档文件，开始转换...", len(docFiles))
-
-			// 转换所有文档为markdown
-			for _, docFile := range docFiles {
-				mdFile := strings.TrimSuffix(docFile, filepath.Ext(docFile)) + ".md"
-				log.Debug("正在转换: %s -> %s", docFile, mdFile)
-
-				if err := utils.ConvertDocToMarkdown(docFile, mdFile); err != nil {
-					return fmt.Errorf("转换文件 %s 失败: %v", docFile, err)
-				}
-			}
+		// 转换文档
+		if err := convertDocToMd(); err != nil {
+			return fmt.Errorf("转换文档失败: %v", err)
 		}
 
 		// 执行git add --all
-		log.Debug("执行 git add --all...")
-		if _, err := utils.ExecCmd("git add --all"); err != nil {
+		if err := gitAdd(); err != nil {
 			return fmt.Errorf("git add 失败: %v", err)
 		}
 
-		// 获取用户输入的commit信息
-		log.Info("请输入本次变更信息:")
-		reader := bufio.NewReader(os.Stdin)
-		commitMsg, err := reader.ReadString('\n')
-		if err != nil {
-			return fmt.Errorf("读取commit信息失败: %v", err)
-		}
-		commitMsg = strings.TrimSpace(commitMsg)
-
-		if commitMsg == "" {
-			return fmt.Errorf("commit信息不能为空")
-		}
-
 		// 执行git commit
-		log.Debug("执行 git commit...")
-		if _, err := utils.ExecCmd(fmt.Sprintf("git commit -m \"%s\"", commitMsg)); err != nil {
+		if err := gitCommit(); err != nil {
 			return fmt.Errorf("git commit 失败: %v", err)
 		}
 
-		log.Debug("文档转换和提交完成")
+		log.Debug("提交完成")
 		return nil
 	}
+}
+
+// gitAdd 执行git add --all
+func gitAdd() error {
+	if _, err := utils.ExecCmd("git add --all"); err != nil {
+		return fmt.Errorf("git add 失败: %v", err)
+	}
+	return nil
+}
+
+// gitCommit 执行git commit
+func gitCommit() error {
+	// 获取用户输入的commit信息
+	log.Info("请输入本次变更信息:")
+	reader := bufio.NewReader(os.Stdin)
+	commitMsg, err := reader.ReadString('\n')
+	if err != nil {
+		return fmt.Errorf("读取commit信息失败: %v", err)
+	}
+	commitMsg = strings.TrimSpace(commitMsg)
+
+	if commitMsg == "" {
+		return fmt.Errorf("commit信息不能为空")
+	}
+
+	// 执行git commit
+	log.Debug("执行 git commit...")
+	if _, err := utils.ExecCmd(fmt.Sprintf("git commit -m \"%s\"", commitMsg)); err != nil {
+		return fmt.Errorf("git commit 失败: %v", err)
+	}
+	return nil
+}
+
+// convertDocToMd 将doc/docx文件转换为markdown
+func convertDocToMd() error {
+	// 扫描doc/docx文件
+	log.Debug("开始扫描文档文件...")
+	docFiles, err := utils.ScanFilesByExt(".", []string{".doc", ".docx"})
+	if err != nil {
+		return fmt.Errorf("扫描文档文件失败: %v", err)
+	}
+
+	if len(docFiles) == 0 {
+		log.Debug("未找到需要转换的文档文件")
+	} else {
+		log.Debug("找到 %d 个文档文件，开始转换...", len(docFiles))
+
+		// 转换所有文档为markdown
+		for _, docFile := range docFiles {
+			mdFile := strings.TrimSuffix(docFile, filepath.Ext(docFile)) + ".md"
+			log.Debug("正在转换: %s -> %s", docFile, mdFile)
+
+			if err := utils.ConvertDocToMarkdown(docFile, mdFile); err != nil {
+				return fmt.Errorf("转换文件 %s 失败: %v", docFile, err)
+			}
+		}
+	}
+	return nil
 }
